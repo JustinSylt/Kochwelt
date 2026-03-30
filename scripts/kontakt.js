16a32f72912f24445892e6d816ee3eae7a092085
JustinSylt <justin.sylt@outlook.de>
2026-03-30 13:26:16 +0200

    contact-form

--- scripts/kontakt.js
+++ scripts/kontakt.js
@@ -20,3 +20,22 @@ function checkForEmptyInput() {
 
   return allValid;
 }
+
+function sendMail(event) {
+  event.preventDefault();
+  const data = new FormData(event.target);
+
+  fetch('https://formspree.io/f/xojpdeyk', {
+    method: 'POST',
+    body: new FormData(event.target),
+    headers: {
+      Accept: 'application/json',
+    },
+  })
+    .then(() => {
+      window.location.href = '../pages/send_mail.html';
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
